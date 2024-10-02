7745cbe842efc3e2a7035d0e354fca87eb85f4ed
Jordi Sanchis Hernandez <juan_@ldmachine.com>
2024-10-02 11:01:27 +0200

    Subir archivos iniciales

---
+++ altruistapp/src/main/java/com/thecorner/altruistapp/Altruistapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.thecorner.altruistapp;
+
+/**
+ *
+ * @author juan_
+ */
+public class Altruistapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
