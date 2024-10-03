45d9d1150171e84d32f35086970fa63ec160bd54
pepe <pepe@gmail.com>
2024-10-03 14:36:27 +0200

    crear articulo

---
+++ altruistapp/src/main/java/Entidades/Articulo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author THECORNER46
+ */
+public class Articulo {
+    
+}
