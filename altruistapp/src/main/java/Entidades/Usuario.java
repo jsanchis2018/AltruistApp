87821ff0c56212e7f5d8ba80a23cf5cc0011648d
Jordi Sanchis Hernandez <juan_@ldmachine.com>
2024-10-04 10:22:51 +0200

    getters y setters clase usuario

--- altruistapp/src/main/java/Entidades/Usuario.java
+++ altruistapp/src/main/java/Entidades/Usuario.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Entidades;
 
-/**
- *
- * @author juan_
- */
 public class Usuario {
+    private int idUsuario;
+    private String nombreUsuario;
+    private String contraseña;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
     
 }
